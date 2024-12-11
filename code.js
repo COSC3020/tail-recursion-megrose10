ec90247dcde17372cb10affd5d9e97753befb46b
megrose10 <157438545+megrose10@users.noreply.github.com>
2024-12-10 22:31:02 -0700

    Create code.js

---
+++ code.js
@@ -0,0 +1,16 @@
+function fib(n) {
+    return fib_acc(n, 1, 1);
+}
+
+function fib_acc(n, accOne, accTwo) {
+    //console.log(accTwo);
+    if(n<=2) {
+        return accTwo;
+    }
+    //console.log(accOne);
+    return fib_acc(n - 1, accTwo, accOne + accTwo);
+}
+
+//console.log(fib(4));
+//console.log(fib(0));
+//console.log(fib(7));
